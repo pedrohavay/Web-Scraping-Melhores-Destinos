ed6786f77671a54f1b91acf54fbf9e00159aaed1
unknown <projetoschan@gmail.com>
2019-08-07 21:01:09 -0300

    Get content and get promos

--- utils.py
+++ utils.py
@@ -7,3 +7,10 @@ class reqMD:
     
     def get_status_code(self):
         return self.req.status_code
+    
+    def get_content(self):
+        return self.content
+
+    def get_promos(self):
+        soup = BeautifulSoup(self.get_content(), 'html.parser')
+        return soup.find_all('div', class_='promocao')
